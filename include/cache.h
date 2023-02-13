07d57a28827a4c705a4587410d0e733e3cd62cff
Keisuke Iida <cmpl.error@gmail.com>
2023-02-14 06:37:45 +0900

    fix: vsm bug

--- include/cache.h
+++ include/cache.h
@@ -3,13 +3,11 @@
 
 #include "types.h"
 
-void dcache_flush_poc(u64 va_start, u64 va_end);
-void cache_sync_pou(u64 va_start, u64 va_end);
+void dcache_flush_poc(void *va_start, void *va_end);
+void cache_sync_pou(void *va_start, void *va_end);
 
-#define dcache_flush_poc_range(v, s)  __dcache_flush_poc_range((u64)(v), (s))
-
-static inline void __dcache_flush_poc_range(u64 va, u64 size) {
-  u64 end = va + size;
+static inline void dcache_flush_poc_range(void *va, u64 size) {
+  void *end = va + size;
   dcache_flush_poc(va, end);
 }
 
